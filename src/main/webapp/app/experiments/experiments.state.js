e1336de0ffd323312c785c500898c04ab6fa7a9b
Angelos Valsamis <ang.valsamis@gmail.com>
2016-11-11 18:19:10 +0200

    Develop busulfan algorithm. Prepare result screen.

--- src/main/webapp/app/experiments/experiments.state.js
+++ src/main/webapp/app/experiments/experiments.state.js
@@ -9,19 +9,58 @@
 
     function stateConfig ($stateProvider) {
         $stateProvider.state('experiments', {
-            parent: 'app',
-            url: '/experiment',
+                parent: 'app',
+                url: '/experiment',
+                data: {
+                    authorities: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/experiments/experiments.html',
+                        controller: 'ExperimentController',
+                        controllerAs: 'vm'
+                    }
+                }, resolve: {
+                    entity: function () {
+                        return {
+                            patient: null,
+                            drug: null,
+                            dosage: 1.1,
+                            concentration1: 0.705,
+                            concentration2: 1.469,
+                            concentration3: 1.263,
+                            concentration4: 1.130,
+                            concentration5: 0.624,
+                            time1: new Date("Thu Jan 01 1970 01:50:00 GMT+0200 (EET)"),
+                            time2: new Date("Thu Jan 01 1970 04:20:00 GMT+0200 (EET)"),
+                            time3: new Date("Thu Jan 01 1970 04:50:00 GMT+0200 (EET)"),
+                            time4: new Date("Thu Jan 01 1970 05:50:00 GMT+0200 (EET)"),
+                            time5: new Date("Thu Jan 01 1970 08:50:00 GMT+0200 (EET)")
+                        }
+
+                    }
+                }
+            }
+        ).state('experiments.result', {
+            parent: 'experiments',
+            url: '/experiment/result',
             data: {
                 authorities: []
             },
-            views: {
-                'content@': {
-                    templateUrl: 'app/experiments/experiments.html',
+            onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/experiments/experiments.result.html',
                     controller: 'ExperimentController',
-                    controllerAs: 'vm'
-                }
-            }
-        });
-    }
-})();
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg'
+                }).result.then(function () {
+                    $state.go('^', {}, {reload: false});
+                }, function () {
+                    $state.go('^');
+                });
+            }]
+        })
+    }})
+();
 
